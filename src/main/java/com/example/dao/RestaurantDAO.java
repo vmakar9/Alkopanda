dd51182e14b4248a3fe421c3305e1a8de7fd39ab
vmakar9 <zmerin212@gmail.com>
2023-04-06 17:49:11 +0300

    1

--- src/main/java/com/example/dao/RestaurantDAO.java
+++ src/main/java/com/example/dao/RestaurantDAO.java
@@ -3,5 +3,8 @@ package com.example.dao;
 import com.example.models.restaurant.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RestaurantDAO extends JpaRepository<Restaurant,Integer> {
+    List<Restaurant> findRestaurantByName(String name);
 }
